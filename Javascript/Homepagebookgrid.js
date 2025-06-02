7d1f3dc96385afa1447a548dcd7605ad1ea8e36e
Wendilidili <jlluan@student.unimelb.edu.au>
2025-06-02 15:15:34 +1000

    Search mini menu complete

--- Javascript/Homepagebookgrid.js
+++ Javascript/Homepagebookgrid.js
@@ -76,18 +76,17 @@ books.forEach((book, index) => {
 
 
 //search results linking to other pages: 
-  const searchBox = document.getElementById('search-input');
+const searchBox = document.getElementById('search-input');
 
-  searchBox.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      e.preventDefault(); // Optional: prevent form submission
-      const query = searchBox.value.trim();
-
-      // Redirect to a page with the search query in URL
-      if (query) {
-        window.location.href = `QBDSearch.html?=${encodeURIComponent(query)}`;
-      }
+searchBox.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    const query = searchBox.value.trim();
+    if (query) {
+      window.location.href = `QBDSearch.html?query=${encodeURIComponent(query)}`;
     }
-  });
+  }
+});
+
 
 
